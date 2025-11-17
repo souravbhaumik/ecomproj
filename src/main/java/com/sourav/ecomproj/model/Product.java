8d58c480d96197c6464cd495a3e19f9a197dd1a0
Sourav Bhaumik <souravbhaumik0812@gmail.com>
2025-11-18 00:06:35 +0530

    Implementing CRUD

--- src/main/java/com/sourav/ecomproj/model/Product.java
+++ src/main/java/com/sourav/ecomproj/model/Product.java
@@ -4,12 +4,12 @@ import java.math.BigDecimal;
 import java.sql.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonValueFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,9 +29,14 @@ public class Product {
     private BigDecimal price;
     private String category;
 
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    // @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
     private Date releaseDate;
     private boolean productAvailable;
     private int stockQuantity;
+
+    private String imageName;
+    private String imageType;
+    @Lob
+    private byte[] imageData;
 
 }
--- src/main/java/com/sourav/ecomproj/service/ProductService.java
+++ src/main/java/com/sourav/ecomproj/service/ProductService.java
@@ -1,9 +1,11 @@
 package com.sourav.ecomproj.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.sourav.ecomproj.model.Product;
 import com.sourav.ecomproj.repo.ProductRepo;
@@ -20,6 +22,19 @@ public class ProductService {
 
     public Product getProductById(int id) {
         return productRepo.findById(id).orElse(null);
+    }
+
+    public Product addOrUpdateProduct( Product product, MultipartFile image ) throws IOException
+    {
+        product.setImageName(image.getOriginalFilename());
+        product.setImageType(image.getContentType());
+        product.setImageData(image.getBytes());
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct( int id )
+    {
+        productRepo.deleteById(id);
     }
 
 }
